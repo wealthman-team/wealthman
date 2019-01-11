dd2e2abd218e3de90db4dccd2b4ad49f825ff22d
i.zubarev <zubarevski@gmail.com>
2019-01-11 14:41:10 +0300

    #comment #comment add href in compareBanner

--- resources/views/components/compareBanner.blade.php
+++ resources/views/components/compareBanner.blade.php
@@ -2,7 +2,7 @@
     <div class="compare-banner__title">
         COMPARE ALL INVESTMENT ADVISORS
     </div>
-    <a class="button button_adaptive">
+    <a class="button button_adaptive" href="{{ route('roboAdvisors') }}">
         Advisor screener
     </a>
 </div>
