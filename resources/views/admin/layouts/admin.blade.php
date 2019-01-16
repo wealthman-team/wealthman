82f0b7e725c92d3a0523cf76415aeaa89775afa3
i.zubarev <zubarevski@gmail.com>
2019-01-16 18:07:57 +0300

    viewport 1310

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="description" content="{{ $pageDescription }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=1310">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- Favicon --}}
     <link rel="apple-touch-icon" sizes="57x57" href="/images/favicons/apple-icon-57x57.png">
